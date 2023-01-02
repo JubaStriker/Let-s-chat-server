925fc5f2e80c93e98704428612744e694c4aac86
JubaStriker <jubastriker@gmail.com>
2023-01-02 11:50:31 +0600

    Chat model added

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { chats } = require('../data/data');
+
+app.get('/chats', async (req, res) => {
+    res.send(chats)
+})
 
 app.get('/', async (req, res) => {
-    res.send('know me server is running');
+    res.send("let's chat server is running");
 })
 
 app.listen(port, () => {
 
-    console.log(`Know me server is running on port ${port}`);
+    console.log(`let's chat server is running on port ${port}`);
 });
